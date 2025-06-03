2b37d41f14cf49c1236dd067435f472e963e7842
maha <d.naveenkumar8@gmail.com>
2025-06-03 21:01:07 +0530

    Clean config and add tests

---
+++ DhanAlgoTrading.Tests/DhanServiceTests.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using DhanAlgoTrading.Api.Services;
+using DhanAlgoTrading.Models.Configuration;
+using DhanAlgoTrading.Models.DhanApi;
+using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.Extensions.Options;
+using Xunit;
+
+namespace DhanAlgoTrading.Tests
+{
+    public class DhanServiceTests
+    {
+        private class FakeHandler : HttpMessageHandler
+        {
+            private readonly HttpResponseMessage _response;
+            public FakeHandler(HttpResponseMessage response)
+            {
+                _response = response;
+            }
+            protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+            {
+                return Task.FromResult(_response);
+            }
+        }
+
+        [Fact]
+        public async Task GetExpiryDatesAsync_ReturnsList()
+        {
+            var json = "{\"data\":[\"2024-06-01\",\"2024-06-08\"]}";
+            var response = new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new StringContent(json)
+            };
+
+            var httpClient = new HttpClient(new FakeHandler(response))
+            {
+                BaseAddress = new Uri("https://api.test/")
+            };
+            var settings = Options.Create(new DhanApiSettings { BaseUrl = "https://api.test/", ClientId = "cid", AccessToken = "token" });
+            var service = new DhanService(httpClient, settings, NullLogger<DhanService>.Instance);
+
+            var result = await service.GetExpiryDatesAsync(new ExpiryListRequestDto());
+
+            Assert.Contains("2024-06-01", result);
+        }
+    }
+}
